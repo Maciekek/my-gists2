c454ad194f813481c47a413df60977de212acfa3
Maciekek <maciekek@gmail.com>
2014-03-15 16:19:56 +0100

    inicjalizacja projektu

---
+++ app/views/gists/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @gist, :id, :snippet, :lang, :description, :date, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyGists::Application.config.secret_key_base = 'a35128a233637a4b08cc3c16ed69e3ce4ec729719bc708daff0c657e3a32989cf20f8665422f3de191e7988ee4c5eb24dd70ff1e3d447bac2c6b7340b5976322'
